453b3cd34afe038a6e36e8b29dfba1b2245b0767
preciousultimate <164312457+preciousultimate@users.noreply.github.com>
2024-04-09 13:56:48 +0100

    Update verification.js

--- verification.js
+++ verification.js
@@ -1,34 +1,35 @@
 // STUDENT VERIFICATION SCRIPT
 let form = document.querySelector("#form");
-let firstName = document.querySelector("#firstname");
-let surName = document.querySelector("#surname");
-let registeredStudent = ["JERRY ID"];
+let studentName = document.querySelector("#student");
+let password = document.querySelector("#password");
+let registeredStudent = ["I0D8J0"];
 let btn = document.querySelector("#btn");
 let errors = document.querySelectorAll(".error")
 form.onsubmit =(eve)=>{
-    if (registeredStudent.includes(firstName.value.trim() + " " +  surName.value.trim())){
+    if (registeredStudent.includes(password.value.trim())){
     }else{
         eve.preventDefault();
-        firstName.style.border ="1px groove hsl(354, 100%, 66%)"
-        surName.style.border ="1px groove hsl(354, 100%, 66%)"
-        errors[2].textContent="This name is not in our database."; 
+        studentName.style.border ="1px groove hsl(354, 100%, 66%)"
+        password.style.border ="1px groove hsl(354, 100%, 66%)"
+        errors[2].textContent=" Not in our database."; 
     }
-if(firstName.value.trim() === ""){
+if(studentName.value.trim() === ""){
     errors[0].textContent="This field is required";
-    firstName.style.border ="1px groove hsl(354, 100%, 66%)"
+    studentName.style.border ="1px groove hsl(354, 100%, 66%)"
     eve.preventDefault();
     errors[2].textContent="";
 }else{
     errors[0].textContent="";
-    firstName.style.border ="1px groove hsl(223, 87%, 63%)"
+    studentName.style.border ="1px groove hsl(223, 87%, 63%)"
 }
-if(surName.value.trim() === ""){
-    errors[1].textContent="This field is required";
-    surName.style.border ="1px groove hsl(354, 100%, 66%)"
+if(password.value.trim() === ""){
+    errors[1].textContent="Password is required";
+    password.style.border ="1px groove hsl(354, 100%, 66%)"
     errors[2].textContent="";
     eve.preventDefault();
 }else{
-     surName.style.border ="1px groove hsl(223, 87%, 63%)"
+     password.style.border ="1px groove hsl(223, 87%, 63%)"
 }
 }
 // END
+
